bd9cb70d1df9a1c369dc0c2771f8e7358e0b0597
ryzenboi98 <joaopro23@gmail.com>
2021-11-10 14:54:36 +0000

    Created functionality to add a stat

--- src/main.py
+++ src/main.py
@@ -1,10 +1,44 @@
 from fastapi.security.api_key import APIKey
 from fastapi import Depends, FastAPI
 
+from sqlalchemy.orm import Session
+from src.database import get_db
+from src.models import Stats
+from src.schemas import CreateStats
+
 from src.helpers import validate_api_key
+from datetime import datetime
+
+from dateutil.parser import parse
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root(api_key: APIKey = Depends(validate_api_key)):
     return {"message": "Hello World"}
+
+@app.post("/stats", status_code=201)
+def create(
+    details: CreateStats,
+    api_key: APIKey = Depends(validate_api_key),
+    db: Session = Depends(get_db),
+):
+
+    stat = Stats(
+        lower=details.lower,
+        higher=details.higher,
+        open=details.open,
+        close=details.close,
+        volume=details.volume,
+        change=details.change,
+        timestamp=details.timestamp
+    ) 
+    
+    db.add(stat)
+    db.commit()
+
+    return {
+        'success': True,
+        "created_id": stat.id
+    }
---
+++ src/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, validator
+from typing import Optional
+
+from dateutil.parser import parse
+
+class CreateStats(BaseModel):
+    lower: float
+    higher: float
+    open: float
+    close: float
+    volume: float
+    change: Optional[float] = None
+    timestamp: str
+
+    @validator('timestamp')
+    def validate_date(cls, v):
+        try:
+            parse(v)
+        except:
+            raise ValueError('Date must be valid')
+            
+        return v
