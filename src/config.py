8c5a82b092beed44b67c27c6ffdcc2c5bc23bcd8
ryzenboi98 <joaopro23@gmail.com>
2021-11-20 16:09:30 +0000

    Update conf files

--- src/config.py
+++ src/config.py
@@ -7,4 +7,4 @@ ENV = os.getenv("ENV", default="dev")
 
 API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = os.getenv("API_KEY_NAME")
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
