0bbb1ff4bf37d582dbd9298692f0f7f335a8ebdb
ryzenboi98 <joaopro23@gmail.com>
2021-12-10 18:24:58 +0000

    Dockerizing the app

--- src/config.py
+++ src/config.py
@@ -7,4 +7,12 @@ ENV = os.getenv("ENV", default="dev")
 
 API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = os.getenv("API_KEY_NAME")
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if ENV == 'PROD':
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres', 'postgresql')
+elif ENV == 'DOCKER':
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('@localhost', '@btc-db')
+    print(SQLALCHEMY_DATABASE_URL)
+else:
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
