e55f9b0c02543f241cdf02d66a1788a0d113fa95
ryzenboi98 <joaopro23@gmail.com>
2022-02-06 02:04:23 +0000

    Updated config

--- src/config.py
+++ src/config.py
@@ -7,11 +7,4 @@ ENV = os.getenv("ENV", default="dev")
 
 API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = os.getenv("API_KEY_NAME")
-
-if ENV == 'PROD':
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres', 'postgresql')
-elif ENV == 'DOCKER':
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('@localhost', '@btc-db')
-else:
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres', 'postgresql')
